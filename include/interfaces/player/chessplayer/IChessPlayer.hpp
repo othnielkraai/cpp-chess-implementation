987c7e8657d5f14ab464230430d07f5a542065cc
Othniel Kraai <kraai241104@gmail.com>
2026-03-31 15:00:39 +0200

    feat: add player.cpp implementation

--- include/interfaces/player/chessplayer/IChessPlayer.hpp
+++ include/interfaces/player/chessplayer/IChessPlayer.hpp
@@ -8,6 +8,7 @@ namespace boardgame::player::chess
     class IChessPlayer : public IPlayer
     {
     public:
+        // Virtual destructor to ensure proper cleanup of derived classes
         virtual ~IChessPlayer() = default;
 
         // Returns true if the player is playing as white, false if playing as black
